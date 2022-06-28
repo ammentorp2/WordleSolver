4c3f877783086059d16b8684699300dca2805da5
Andrew Ammentorp <andrew_ammentorp1@baylor.edu>
2022-06-28 18:12:13 -0500

    about page updated w/links

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,12 +2,44 @@ import React from 'react';
 import { Button, Grid, MenuItem, Select, TextField, Typography } from "@mui/material"
 import { useNavigate } from "react-router-dom";
 import { Header } from '../components/Header';
+import { Collapse } from '@mui/material';
 
 export const AboutPage = () => {
     return(
         <div>
             <Header />
-            Coming soon...
+            <Grid container justifyContent="center" direction={'column'} spacing={5}>
+                <Grid item>
+                    <Typography variant="h4">
+                        How does the app work?
+                    </Typography>
+                    <Typography paragraph>
+                        This app was made with a ReactJS frontend (using create-react-app) and Python Flask endpoints.
+                        From there, the server was seperated from the frontend and each were deployed using Heroku. 
+                    </Typography>
+                </Grid>
+                <Grid item>
+                    <Typography variant="h4">
+                        App history
+                    </Typography>
+                    <Typography paragraph>
+                        Originally, I just just made this as a python console app inspired by (https://replit.com/@jamesabela/WordleSolver).
+                        Quickly I wanted the program to calculate a suggested guess based on the available words. 
+                        But how can one quantify that? The initial way
+                        I calculated this by assigning each word a "score" , if the letter hadn't appeared in the word add its
+                        frequency score (found here: https://www3.nd.edu/~busiforc/handouts/cryptography/letterfrequencies.html). 
+                        Then, the word with the highest score would become the sugested guess.
+                        From there, I created functionality that given the correct word would solve the word (aka always using 
+                        the suggested guess). After that I expanded upon the formula, not only factoring in how often the word appears,
+                        but how often it appears <i>in each position in the word</i>   
+                        (http://www.viviancook.uk/SpellStats/LetFreqByWordPosition.html). For example, using the old 
+                        formula <b>Irate</b> was the suggested first guess. While containing common letters, not many words start with 'I' 
+                         (relatively speaking).
+                        Thus factoring in the start,middle, and medial positions of each letter <b>Atone</b> is new first suggested guess.
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Collapse>Test</Collapse>
         </div>
     )
 }
